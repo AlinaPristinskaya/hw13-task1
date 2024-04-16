3dce2eb3f4a66a5cda3854457261510d90ae675a
Alina Prystynska <pristinskayaalina9@gmail.com>
2024-04-16 23:02:33 +0100

    all

--- task2.js
+++ task2.js
@@ -3,14 +3,10 @@ async function fetchRandomCatGif() {
     try {
       // API URL that returns a random cat GIF
       const url = 'http://thecatapi.com/api/images/get?format=src&type=gif';
+      
   
       // Fetch the URL directly; the API is configured to redirect to a GIF
       const response = await fetch(url);
-  
-      // Check if the fetch was successful
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
   
       // Since the API redirects directly to the GIF, we use the final URL
       console.log(response.url);
